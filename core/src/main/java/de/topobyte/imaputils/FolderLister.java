869705d4595d1325ce2c47dfdb60174ef33d1584
Sebastian Kürten <sebastian@topobyte.de>
2019-02-09 12:06:38 +0100

    Rename 'util' to 'utils' everywhere

--- core/src/main/java/de/topobyte/imaputils/FolderLister.java
+++ core/src/main/java/de/topobyte/imaputils/FolderLister.java
@@ -15,7 +15,7 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with imap-utils. If not, see <http://www.gnu.org/licenses/>.
 
-package de.topobyte.imaputil;
+package de.topobyte.imaputils;
 
 import javax.mail.Folder;
 import javax.mail.MessagingException;
